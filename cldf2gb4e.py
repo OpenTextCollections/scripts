56457288cfbc4eb922c7ad013867c5fec37ab5fb
glottotopia <sebastian.nordhoff@glottotopia.de>
2023-11-13 17:03:29 +0100

    use better filename; better escaping of latex special chars

--- cldf2gb4e.py
+++ cldf2gb4e.py
@@ -23,22 +23,28 @@ end_document="\\end{document}"
 
 
 filename = sys.argv[1]
-with open(f"{filename}.tex","w") as tex_file:
-    tex_file.write(preamble)
-    with open(filename, mode='r') as csv_file:
-        csv_reader = csv.DictReader(csv_file)
-        for row in csv_reader:
-            tex_file.write('\\ea\\label{ex:%s}\n' % row["ID"])
-            vernacular = row["Analyzed_Word"].strip()
-            vernacular_words = vernacular.split("\t")
-            recomposed_string = "\t".join(["{%s}"%w if " " in w else w for w in vernacular_words])
-            tex_file.write(f'\\gll {recomposed_string}\\\\\n')
-            gloss = row["Gloss"]
-            allcapsglosses = re.findall("([A-Z][A-Z]+)",gloss)
-            for match in  sorted(allcapsglosses):
-                gloss=gloss.replace(match, "\\textsc{%s}"%match.lower())
-            gloss=gloss.replace(" ", "\\_")
-            tex_file.write(f'     {gloss}\\\\\n')
-            tex_file.write(f"""\\glt `{row["Translated_Text"]}'\n""")
-            tex_file.write("\\z\n\n")
-    tex_file.write(end_document)
+if not filename.endswith("csv"):
+    print("please provide a file of type csv")
+else:
+    tex_filemame = filename[0:-4]+".tex"
+    print(tex_filemame)
+    with open(tex_filemame,"w") as tex_file:
+        tex_file.write(preamble)
+        with open(filename, mode='r') as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+            for row in csv_reader:
+                tex_file.write('\\ea\\label{ex:%s}\n' % row["ID"])
+                vernacular = row["Analyzed_Word"].strip()
+                vernacular_words = vernacular.split("\t")
+                recomposed_string = "\t".join(["{%s}"%w if " " in w else w for w in vernacular_words]).replace("&","\\&").replace("#","\\#")
+                tex_file.write(f'\\gll {recomposed_string}\\\\\n')
+                gloss = row["Gloss"]
+                allcapsglosses = re.findall("([A-Z][A-Z]+)",gloss)
+                for match in  sorted(allcapsglosses):
+                    gloss=gloss.replace(match, "\\textsc{%s}"%match.lower())
+                gloss=gloss.replace(" ", "\\_").replace("&","\\&")
+                tex_file.write(f'     {gloss}\\\\\n')
+                processed_translation = row["Translated_Text"].replace("&","\\&").replace("#","\\#")
+                tex_file.write(f"""\\glt `{processed_translation}'\n""")
+                tex_file.write("\\z\n\n")
+        tex_file.write(end_document)
